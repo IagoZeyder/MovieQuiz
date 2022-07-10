876326bbc11d9d8a4c63099427e90f14c5896692
Iago Thebano <iagothebano2001@gmail.com>
2022-07-10 14:13:34 -0300

    Iniciando a segunda questão

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var respostas = {
     primeiraQuestao: 2001,
-    seguntaQuestao: 2002
+    seguntaQuestao: Avatar
 }
 
 function conferirResposta(){
     var resposta = document.getElementById('input_1').value
+    var resposta = document.getElementById('input_2').value
     if(resposta == respostas.primeiraQuestao[0]){
         window.alert('Acertou')
     }
