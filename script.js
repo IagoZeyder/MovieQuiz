d6e5b3a8d250238a540d5c8428662099c91c5005
Iago Thebano <iagothebano2001@gmail.com>
2022-07-10 15:18:59 -0300

    Segunda questão criada

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 var respostas = {
-    primeiraQuestao: 2001,
-    seguntaQuestao: Avatar
+    primeiraQuestao: '2001',
+    seguntaQuestao: ['Avatar','Vingadores Ultimato']
+}
+var resposta
+var resposta2
+function pegarRespostas(){
+    resposta = document.getElementById('input_1').value
+    resposta2 = document.getElementById('input_2').value
 }
 
 function conferirResposta(){
-    var resposta = document.getElementById('input_1').value
-    var resposta = document.getElementById('input_2').value
-    if(resposta == respostas.primeiraQuestao[0]){
+    pegarRespostas();
+    if(resposta == respostas.primeiraQuestao){
         window.alert('Acertou')
     }
+    else if (resposta == null){        
+
+    }
+
     else{
         window.alert('incorreto')
     }
+
+    if(resposta2 == respostas.seguntaQuestao[0] ){
+        window.alert('Acertou')
+    }
+
+    else if (resposta2 ==  null){        
+
+    }
+
+    else{
+        window.alert('incorreto')
+    }
+
 }
