c76e86b7f758f50f9aa4f594dacf991e43c7f67f
Iago Thebano <iagothebano2001@gmail.com>
2022-07-12 12:45:44 -0300

    Criando terceira pergunta e utilizando o loop for para automatizar as respostas

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-var respostas = {
-    primeiraQuestao: '2001',
-    seguntaQuestao: ['Avatar','Vingadores Ultimato']
+// Array com as repostas das perguntas
+
+var resposta = {
+    resposta1: ['',''],
+    resposta2: ['',''],
+    resposta3:['','']
 }
-var resposta
-var resposta2
-function pegarRespostas(){
-    resposta = document.getElementById('input_1').value
-    resposta2 = document.getElementById('input_2').value
+
+var questoesResp = {
+    primeiraQuestao: ['2001',''],
+    seguntaQuestao: ['Avatar','Vingadores Ultimato'],
+    terceiraQuestao:['Homem de Ferro','Homem Aranha',]
+}
+function pegarRespostas(){    
+        resposta[0] = document.getElementById('input_1').value
+        resposta[1] = document.getElementById('input_2').value
+        resposta[2] = document.getElementById('input_3').value    
 }
 
 function conferirResposta(){
     pegarRespostas();
-    if(resposta == respostas.primeiraQuestao){
-        window.alert('1 - Questão: Acertou')
+    var cont = 1
+    for(var i = 0; i<3; i++){
+        console.log(resposta[i],[i])
+        console.log('Respostas'+questoesResp[i],[i])
+        if(resposta[i],[i] == questoesResp[i],[i]){
+            window.alert(cont +' - Questão: Acertou')
+            cont++
+         }
+        else if (resposta[i],[i] == ''){        
+            window.alert('incorreto')
+            cont++
+        }
+
+        else if(resposta[i],[i] != questoesResp[i],[i]){
+            window.alert('incorreto')
+            cont++
+        }
+  
     }
-    else if (resposta == null){        
-
-    }
-
-    else{
-        window.alert('incorreto')
-    }
-
-    if(resposta2 == respostas.seguntaQuestao[0] ){
-        window.alert('2 - Questão: Acertou')
-    }
-
-    else if (resposta2 ==  null){        
-
-    }
-
-    else{
-        window.alert('incorreto')
-    }
-
 }
