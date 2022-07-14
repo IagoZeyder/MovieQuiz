a5317dd4f910653636053a3ea6de903fa8b1a427
Iago Thebano <iagothebano2001@gmail.com>
2022-07-13 22:38:21 -0300

    Quesao 4 feita e criado um novo for

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // Array com as repostas das perguntas
 
 var resposta = {
-    respostas: ['','','']
+    respostas: ['','','','']
     
 }
 
 var questoesResp = {
-    questoes: ['2001','Avatar','Homem de Ferro']   
+    questoes: ['2001','Avatar','Homem de Ferro','2015']   
 }
 function pegarRespostas(){    
         resposta.respostas[0] = document.getElementById('input_1').value             
         resposta.respostas[1] = document.getElementById('input_2').value       
-        resposta.respostas[2] = document.getElementById('input_3').value            
+        resposta.respostas[2] = document.getElementById('input_3').value 
+        resposta.respostas[3] = document.getElementById('input_4').value            
 }
 
 function conferirResposta(){
     pegarRespostas();
     var cont = 1
-    for(var i = 0; i<3; i++){
+    for(var i in questoesResp.questoes){
         console.log(resposta.respostas[i])
         console.log('Respostas: '+questoesResp.questoes[i])
         if(resposta.respostas[i] == questoesResp.questoes[i]){
